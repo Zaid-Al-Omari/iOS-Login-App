cd56d2408ff4ab51d3305158150b749500f63ec3
student <student@CR3309i.local>
2016-04-15 05:23:02 +0800

    Initial Commit

---
+++ Login/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by student on 15/4/16.
+//  Copyright © 2016 ISS-MWAD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
